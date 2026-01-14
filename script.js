56f7556682eb735e78da4a802c89e65d66c515a7
alisschavez07-rgb <alisschavez07@gmail.com>
2026-01-14 14:00:59 -0500

    Implement course approval toggle with localStorage

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Archivo listo para futuras mejoras
+// Selecciona todos los cursos
+const cursos = document.querySelectorAll(".curso");
+
+// Cargar cursos aprobados guardados
+const aprobadosGuardados = JSON.parse(localStorage.getItem("cursosAprobados")) || [];
+
+cursos.forEach(curso => {
+  if (aprobadosGuardados.includes(curso.innerText)) {
+    curso.classList.add("aprobado");
+  }
+
+  curso.addEventListener("click", () => {
+    curso.classList.toggle("aprobado");
+    guardarEstado();
+  });
+});
+
+function guardarEstado() {
+  const aprobados = [];
+  cursos.forEach(curso => {
+    if (curso.classList.contains("aprobado")) {
+      aprobados.push(curso.innerText);
+    }
+  });
+  localStorage.setItem("cursosAprobados", JSON.stringify(aprobados));
+}
